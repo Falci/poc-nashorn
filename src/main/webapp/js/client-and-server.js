b77547bc11892e893441c935b7a2b58834bbbf30
Fernando Falci <falci@falci.me>
2015-11-15 16:26:25 -0200

    :lipstick: translated the Portuguese code

--- src/main/webapp/js/client-and-server.js
+++ src/main/webapp/js/client-and-server.js
@@ -3,18 +3,18 @@ function getErrors(form) {
   var errors = [];
 
   if(!form.name.length){
-    errors.push('Nome é obrigatório');
+    errors.push('Name is mandatory');
   }
 
   if(!form.email.length){
-    errors.push('Email é obrigatório');
+    errors.push('Email is mandatory');
 
   } else {
 
     if(!/@falci\.me$/.test(form.email)){
-      errors.push('Email precisa ser @falci.me');
+      errors.push('Email must be @falci.me');
     }
-    
+
   }
 
   return errors;
